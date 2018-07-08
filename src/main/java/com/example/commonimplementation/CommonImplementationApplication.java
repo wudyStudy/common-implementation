68e06098a7ba1c42da9bf6c483d23cd10fe06eb0
zhangminghui <zhangminghui@u51.com>
2018-07-08 21:50:11 +0800

    init

---
+++ src/main/java/com/example/commonimplementation/CommonImplementationApplication.java
@@ -0,0 +1,12 @@
+package com.example.commonimplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonImplementationApplication.class, args);
+	}
+}
